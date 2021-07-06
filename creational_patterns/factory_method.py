5d3428a1e5a0c729bd4b6f4beb69fd5ba15112f4
Максим Башкатов <maksymbashkatov@gmail.com>
2021-07-06 20:01:43 +0300

    Module for Abstract factory was added.

--- creational_patterns/factory_method.py
+++ creational_patterns/factory_method.py
@@ -4,28 +4,28 @@ from abc import ABC, abstractmethod
 class FilmStudio(ABC):
     """
     Базовый класс Создатель. Объявляет фабричный метод, возвращает новые объекты фильмов в зависимости от студии с
-    с котрой подписан контракт.
+    с которой подписан контракт.
     """
     @abstractmethod
-    def create_film(self):
+    def _create_film(self):
         """
         Фабричный метод.
-        :return: объект фильма
+        :return: в переопределённых будет возвращать объект фильма от нужной компании
         """
         pass
 
     def basic_business_logic(self):
         # Вызов фабричного метода, чтобы получить объект-фильм.
-        film = self.create_film()
+        film = self._create_film()
         # Сообщение о том от какой компании будет доставлен в кинотеатр фильм.
-        print(film.go_to_theater())
+        print(film._go_to_theater())
 
 class UniversalPictures(FilmStudio):
-    def create_film(self) -> Film:
+    def _create_film(self) -> Film:
         return UniversalPicturesFilm()
 
 class NewLineCinema(FilmStudio):
-    def create_film(self) -> Film:
+    def _create_film(self) -> Film:
         return NewLineCinemaFilm()
 
 class Film(ABC):
@@ -33,18 +33,18 @@ class Film(ABC):
     Базовый класс продукта. Определяет общий интерфейс фильмов.
     """
     @abstractmethod
-    def go_to_theater(self):
+    def _go_to_theater(self):
         """
         :return: строку с информаией о том от какой компании будет доставлен в кинотеатр фильм
         """
         pass
 
 class UniversalPicturesFilm(Film):
-    def go_to_theater(self) -> str:
+    def _go_to_theater(self) -> str:
         return 'В кинотеатр отправляется фильм компании UniversalPictures.'
 
 class NewLineCinemaFilm(Film):
-    def go_to_theater(self) -> str:
+    def _go_to_theater(self) -> str:
         return 'В кинотеатр отправляется фильм компании NewLineCinema.'
 
 # tests
